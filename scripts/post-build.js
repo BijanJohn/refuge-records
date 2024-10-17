4104c2a46dbb36ef72005d117b9ca73fca303c78
Bijan <34772523+BijanJohn@users.noreply.github.com>
2024-10-17 04:57:44 -0500

    fix script

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -3,9 +3,26 @@ const path = require('path');
 
 // Define the file path and content
 const filePath = path.join(__dirname, '../public/page-data/sq/d/1181966677.json');
-const fileContent = JSON.stringify({
-  message: '{"data":{"emiliaConfig":{"name":"RefugeRecords","location":"Austin","socialMedia":[{"href":"https://www.instagram.com/lindseyjeremys/","title":"Instagram"},{"href":"https://www.discogs.com/user/jeremyslindsey","title":"Discogs"}],"showThemeAuthor":true,"assetsPath":"content/assets"}}}'
-}, null, 2);
+const fileContent = {
+    data: {
+      emiliaConfig: {
+        name: "RefugeRecords",
+        location: "Austin",
+        socialMedia: [
+          {
+            href: "https://www.instagram.com/lindseyjeremys/",
+            title: "Instagram"
+          },
+          {
+            href: "https://www.discogs.com/user/jeremyslindsey",
+            title: "Discogs"
+          }
+        ],
+        showThemeAuthor: true,
+        assetsPath: "content/assets"
+      }
+    }
+  };
 
 // Ensure the directory exists
 const dirPath = path.dirname(filePath);
